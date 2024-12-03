1413872c8ad0cfc66421d782fd2e3a7bf7bea66a
Leandro <leandrolucas7@hotmail.com>
2024-12-03 18:22:59 -0300

    refatoracao / remocao dos pre-req / inputs de dados seguem ordem fixa/ melhora nos comentarios

--- main.py
+++ main.py
@@ -9,22 +9,27 @@ def main():
 
     ### LISTAS PARA ARMAZENAMENTO ###
     info = [cargo, responsavel, semestre, ano, local]
+    # coordenador: ["matricula", "nome", "departamento", "posição", [turma]]
     coordenadores = []
+    # professor: ["matricula", "nome", "departamento", "posição", [turma], ["seg", "qui"]]
     professores = []
+    # aluno: ["matricula", "nome", "departamento", "posição", [turma], ["seg", "qui"]]
     alunos = []
+    # turma: ["nome", "departamento", "coordenador", ""]
     turmas = []
 
-    ### COLETAR DADOS PESSOAIS ###
-    CATEGORIAS = ["COORDENADORES", "PROFESSORES", "ALUNOS"]
-    for _ in range(len(CATEGORIAS)):
-        tipo = input()
+    ### COLETAR / DECIFRAR DADOS PESSOAIS ###
+    posicoes = ["coordenador", "professor", "aluno"]
+    for posicao in posicoes:
+        # Indicador da categoria/posição da sequência atual
+        input()
         # Os inputs da categoria encerram com a palavra 'fim'
         dados = input()
         while dados != "fim":
             dados = dados.split(",")
             # Decifrar dados privados (Matrícula e Nome)
             # Coordenadores e professores tem uma camada extra de segurança (Cifra de Substituição)
-            if tipo == CATEGORIAS[0] or tipo == CATEGORIAS[1]:
+            if posicao == posicoes[0] or posicao == posicoes[1]:
                 dados[0] = criptografia_substituicao(dados[0])
                 dados[1] = criptografia_substituicao(dados[1])
             # Criptografia Geral (Cifra de César). O último dígito da matrícula é o fator de rotação 
@@ -34,46 +39,129 @@ def main():
             dados[0] = dados[0][:-1] + str(rotacao)
             dados[1] = criptografia_cesar(dados[1], -rotacao)
 
-            # Adidionar sub-lista para turmas e horários
+            # Adidionar posição e sub-listas para turmas e dias
+            dados.append(posicao)
             dados.append([])
             dados.append([])
             # Adidionar listas com informações pessoais as 'super' listas correspondentes
-            if tipo == CATEGORIAS[0]:
+            if posicao == posicoes[0]:
                 # Remover sub-lista de horários do coordenador
                 dados.pop()
                 coordenadores.append(dados)
-            elif tipo == CATEGORIAS[1]:
+            elif posicao == posicoes[1]:
                 professores.append(dados)
-            elif tipo == CATEGORIAS[2]:
+            elif posicao == posicoes[2]:
                 alunos.append(dados)
             dados = input()
 
     ### INICIAR SISTEMA DE AÇÕES ###
     # Tipos de inputs:
         # Encerramento: "FIM"
-        # Validação: POSIÇÃO AÇÃO MATRÍCULA
+        # Comando: POSIÇÃO AÇÃO MATRÍCULA
     # Posições válidas: "C" (Coordenador) "P" (Professor), "A" (Aluno)
     posicoes_validas = ["C", "P", "A"]
     acoes_validas = ["m", "i"]
-    validacao = input()
-    while validacao != "FIM":
-        posicao, acao, matricula = validacao.split()
+    comando = input()
+    while comando != "FIM":
+        posicao, acao, matricula = comando.split()
         # Verificar válidade da posição e ação
         if not posicao in posicoes_validas:
-            imprimir_erro("Posicao", posicao)
+            argumento_invalido("Posicao", posicao)
         elif not acao in acoes_validas:
-            imprimir_erro("Acao", acao)
+            argumento_invalido("Acao", acao)
         # Passar execução para as funções de controle
         elif posicao == "C":
-            #controlador_coordenador(acao, matricula, coordenadores, turmas, info)
-            controlador("Coordenador", acao, matricula, coordenadores, turmas, info)
+            controlador( acao, matricula, coordenadores, turmas, info)
         elif posicao == "P":
-            #controlador_professor(acao, matricula, professores, turmas, info)
-            controlador("Professor", acao, matricula, professores, turmas, info)
+            controlador( acao, matricula, professores, turmas, info)
         elif posicao == "A":
-            #controlador_aluno(acao, matricula, alunos, turmas, info)
-            controlador("Aluno", acao, matricula, alunos, turmas, info)
-        validacao = input()
+            controlador( acao, matricula, alunos, turmas, info)
+        comando = input()
+
+
+# FUNÇÃO DE CONTROLE ----------------------------------------------------------------- #
+
+def controlador(acao:str, matricula:str, pessoas:list, turmas:list, info:list)->None:
+    # Verificar existência da pessoa com base na posição/matrícula recebida
+    pessoa = busca_2d(matricula, pessoas)
+    if not pessoa:
+        argumento_invalido("Matrícula", matricula)
+        return
+    # Executar ações com base no chamador correto
+    posicoes = ["coordenador", "professor", "aluno"]
+    if acao == "i":
+        imprimir_turmas(pessoa, info)
+    elif acao == "m":
+        nomes_das_turmas = [item[0] for item in turmas]
+        turma = []
+        if pessoa[3].lower()  == posicoes[0]:
+            turma = montar_turma(pessoa)
+            turmas.insert(insercao_binaria(turma[0], nomes_das_turmas), turma)
+        elif pessoa[3].lower() == posicoes[1]:
+            turma = ministrar_turma(pessoa, turmas)
+        elif pessoa[3].lower() == "aluno":
+            turma = matricular_turma(pessoa, turmas)
+        # Inserir turma de forma ordenada na lista de turmas da pessoa atual
+        if turma:
+            pessoa[4].insert(insercao_binaria(turma[0], nomes_das_turmas), turma)
+
+# ------------------------------------------------------------------------------------- #
+
+
+# FUNÇÕES DE AÇÕES -------------------------------------------------------------------- #
+def imprimir_turmas(pessoa:list, info:list)->None:
+    imprimir_cabecalho(info[4], pessoa[2], pessoa[1], pessoa[3])
+    contador = 0
+    for turma in pessoa[4]:
+        if turma_valida(turma):
+            contador += 1
+            print()
+            print(f"#{contador:02}: {turma[0].upper()}")
+            print(f"---> Professor(a): {turma[3]}")
+            print(f"---> Horario: {'/'.join(turma[5])}")
+            print(f"---> Matriculados: {len(turma[4])} aluno(s)")
+    print()
+    print(f"==> {contador} turmas ativa(s).")
+    print()
+    imprimir_rodape(info[1], info[0], info[3], info[2])
+
+
+def montar_turma(coordenador:list)->list:
+    nome, dias = input().split(",")
+    turma = [nome, coordenador[2], coordenador[1], None, [], dias.split("/")]
+    return turma
+
+
+def ministrar_turma(professor:list, turmas:list)->list:
+    nome_da_turma = input()
+    # Verificar existência da turma
+    turma = get_turma(nome_da_turma, turmas)
+    if not turma:
+        return []
+    # Verificar se a turma está disponível para o professor
+    if not turma_compativel(professor, turma, tem_professor=False):
+        return []
+    # Adicionar professor na turma e ocupar dias do professor
+    turma[3] = professor[1]
+    professor[5] += turma[5]
+    return turma
+
+
+def matricular_turma(aluno:list, turmas:list)->list:
+    nome_da_turma = input()
+    # Verificar existência da turma
+    turma = get_turma(nome_da_turma, turmas)
+    if not turma:
+        return []
+    # Verificar se a turma está disponível para o aluno
+    if not turma_compativel(aluno, turma):
+        return []
+    # Adicionar aluno na turma e ocupar dias do aluno
+    turma[4].append(aluno[1])
+    aluno[5] += turma[5]
+    return turma
+
+# ------------------------------------------------------------------------------------- #
 
 
 # FUNÇÕES DE BUSCA / INSERÇÃO --------------------------------------------------------- #
@@ -133,7 +221,7 @@ def get_posicao_sigla(s:str)->int:
             return ultima_aspas - contagem - 1
 
 
-def busca_2d(elemento:str, l:list, sub_posicao:int)->list:
+def busca_2d(elemento:str, l:list, sub_posicao:int=0)->list:
     for sub_lista in l:
         if elemento == sub_lista[sub_posicao]:
             return sub_lista
@@ -160,163 +248,20 @@ def busca_binaria(item:str, itens:list, baixo:int, alto)->int:
     if item.lower() == itens[meio].lower():
         return meio
     elif item.lower() > itens[meio].lower():
-        busca_binaria(item, itens, meio+1, alto)
+        return busca_binaria(item, itens, meio+1, alto)
     else:
-        busca_binaria(item, itens, baixo, meio-1)
-
-# ------------------------------------------------------------------------------------- #
+        return busca_binaria(item, itens, baixo, meio-1)
 
 
-# FUNÇÕES DE CONTROLE ----------------------------------------------------------------- #
-
-def controlador(posicao:str, acao:str, matricula:str, pessoas:list, turmas:list, info:list)->None:
-    # Verificar existência da pessoa com base na posição/matrícula recebida
-    pessoa = busca_2d(matricula, pessoas, 0)
-    if not pessoa:
-        imprimir_erro("Matrícula", matricula)
-        return
-
-    # Executar ações com base no chamador correto
-    if acao == "i":
-        imprimir_turmas(pessoa, posicao, info)
-    elif acao == "m":
-        if posicao.lower()  == "coordenador":
-            turma = montar_turma(pessoa)
-            nomes_das_turmas = [item[0] for item in turmas]
-            turmas.insert(insercao_binaria(turma[0], nomes_das_turmas), turma)
-        elif posicao.lower() == "professor":
-            turma = ministrar_turma(pessoa, turmas)
-        elif posicao.lower() == "aluno":
-            turma = matricular_turma(pessoa, turmas)
-
-
-
-def controlador_coordenador(acao:str, matricula:str, coordenadores:list, turmas:list, info:list)->None:
-    # Verificar existência do coordenador
-    coordenador = busca_2d(matricula, coordenadores, 0)
-    if not coordenador:
-        imprimir_erro("Matricula", matricula)
-        return
-    # Executar ação
-    if acao == "i":
-        imprimir_turmas(coordenador, "Coordenador", info)
-    elif acao == "m":
-        turma = montar_turma(coordenador)
-        nomes_das_turmas = [item[0] for item in turmas]
-        turmas.insert(insercao_binaria(turma[0], nomes_das_turmas), turma)
-
-
-def controlador_professor(acao:str, matricula:str, professores:list, turmas:list, info:list)->None:
-    professor = busca_2d(matricula, professores, 0)
-    if not professor:
-        imprimir_erro("Matricula", matricula)
-        return
-    if acao == "i":
-        imprimir_turmas(professor, "Professor", info)
-    elif acao == "m":
-        turma = ministrar_turma(professor, turmas)
-
-
-def controlador_aluno(acao:str, matricula:str, alunos:list, turmas:list, info:list)->None:
-    aluno = busca_2d(matricula, alunos, 0)
-    if not aluno:
-        imprimir_erro("Matricula", matricula)
-        return
-    if acao == "i":
-        imprimir_turmas(aluno, "Aluno", info)
-    elif acao == "m":
-        turma = matricular_turma(aluno, turmas)
-
-# ------------------------------------------------------------------------------------- #
-
-
-# FUNÇÕES DE AÇÕES -------------------------------------------------------------------- #
-def imprimir_turmas(pessoa:list, posicao:str, info:list)->None:
-    imprimir_cabecalho(info[4], pessoa[2], pessoa[1], posicao)
-    contador = 0
-    for turma in pessoa[3]:
-        if validar_turma(turma):
-            contador += 1
-            print()
-            print(f"{contador}- {turma[0].upper()}")
-            print(f"--> Professor(a): {turma[3]}")
-            print(f"--> Horario: {'/'.join(turma[5])}")
-            print(f"--> Matriculados: {len(turma[4])} aluno(s)")
-    print()
-    print(f"==> {contador} turmas ativa(s).")
-    print()
-    imprimir_rodape(info[1], info[0], info[3], info[2])
-
-
-def montar_turma(coordenador:list)->list:
-    nome, dias, requisito = input().split(",")
-    dias = dias.split("/") # Ex: ['seg', 'qua']...
-    # Nome, Departamento, Coordenador, Professor, Alunos, Dias, Requisitos
-    turma = [nome, coordenador[2], coordenador[1], None, [], dias, requisito]
-    # Inserir turma de forma ordenada na lista de turmas do coordenador
-    turmas = coordenador[3]
-    nomes_das_turmas = [item[0] for item in turmas]
-    turmas.insert(insercao_binaria(nome, nomes_das_turmas), turma)
-    return turma
-
-def ministrar_turma(professor:list, turmas:list)->list:
-    nome_da_turma = input()
-    # Verificar existência da turma
+def get_turma(nome_da_turma:str, turmas:list)->list:
     nomes_das_turmas = [item[0] for item in turmas]
     indice_turma = busca_binaria(nome_da_turma, nomes_das_turmas, 0, len(nomes_das_turmas))
     if indice_turma == -1:
-        print("turma não existe") # Consertar mensagem de erro
+        print("Turma inexistente.")
         return []
-    
-    turma = turmas[indice_turma]
-    # Verificar se departamentos são iguais
-    if professor[2].lower() != turma[1].lower():
-        print("professor não está no departamento da turma") # Consertar mensagem de erro
-        return []
-    # Verificar se turma já tem professor
-    if turma[3]:
-        print("turma já tem professor") # Consertar mensagem de erro
-        return []
-    # Verificar dias da turma
-    if not dias_compativeis(professor[4], turma[5]):
-        print("Conflito de datas") # Consertar mensagem de erro
-        return []
-    
-    # Operação válida
-    turma[3] = professor[1]
-    professor[4] += turma[5]
-    professor[3].append(turma)
-    return turma
+    return turmas[indice_turma]
 
 
-def matricular_turma(aluno:list, turmas:list)->list:
-    nome_da_turma = input()
-    # Verificar existência da turma
-    nomes_das_turmas = [item[0] for item in turmas]
-    indice_turma = busca_binaria(nome_da_turma, nomes_das_turmas, 0, len(nomes_das_turmas))
-    if indice_turma == -1:
-        print("turma não existe") # Consertar mensagem de erro
-        return []
-    
-    turma = turmas[indice_turma]
-    # Verificar se departamentos são iguais
-    if aluno[2].lower() != turma[1].lower():
-        print("aluno não está no departamento da turma") # Consertar mensagem de erro
-        return []
-    # Verificar se turma já tem professor
-    if not turma[3]:
-        print("turma não tem professor") # Consertar mensagem de erro
-        return []
-    # Verificar dias da turma
-    if not dias_compativeis(aluno[4], turma[5]):
-        print("Conflito de datas") # Consertar mensagem de erro
-        return []
-    
-    # Operação válida
-    turma[4].append(aluno[1])
-    aluno[4] += turma[5]
-    aluno[3].append(turma)
-    return turma
 # ------------------------------------------------------------------------------------- #
 
 
@@ -360,14 +305,25 @@ def criptografia_substituicao(s:str)->str:
     return resultado
 
 
-def validar_turma(turma:list)->bool:
+def turma_valida(turma:list)->bool:
     # Existe professor e aluno na turma
     return turma[3] and len(turma[4]) > 0
 
 
-def dias_compativeis(reservado:list, novo:list)->bool:
-    for dia in novo:
-        if dia in reservado:
+def turma_compativel(pessoa:list, turma:list, tem_professor=True)->bool:
+    # Verificar se departamentos são iguais
+    if pessoa[2].lower() != turma[1].lower():
+        print("Turma de departamento diferente.") 
+        return False
+    # Verificar se turma já tem professor
+    if turma[3] and not tem_professor:
+        print("Turma já possui professor.")
+        return False
+    elif not turma[3] and tem_professor:
+        print("Turma não possui professor.")
+    # Verificar conflito de dias
+    for dia in turma[5]:
+        if dia in pessoa[4]:
             return False
     return True
 
@@ -376,7 +332,7 @@ def dias_compativeis(reservado:list, novo:list)->bool:
 
 # FUNÇÕES DE IMPRESSÃO --------------------------------------------------------------------- #
 
-def imprimir_erro(tipo:str, arg:str)->None:
+def argumento_invalido(tipo:str, arg:str)->None:
     print(f"{tipo} '{arg}' invalido(a).")
 
 
@@ -391,6 +347,7 @@ def imprimir_rodape(nome:str, cargo:str, ano:str, semestre:str)->None:
     print(f'"{nome.title()} - {cargo.capitalize()} ({ano}.{semestre})"')
     print("<<<<<")
 # ------------------------------------------------------------------------------------- #
+
 
 if __name__ == "__main__":
     main()
